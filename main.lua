053fb9a3fc11c4248e616e9dbfa9ab9f6bb7a05c
Felix JC <felixjcalvo@gmail.com>
2020-07-26 21:26:13 +0200

    First steps

---
+++ main.lua
@@ -0,0 +1,15 @@
+math.randomseed( os.time() )
+
+list1 = {"Chicken breast", "Turkey", "Egg", "Salmon", "Lean Steak", "Shrimp", "Tofu"}
+list2 = {"Broccoli", "Zucchini", "Squash", "Peppers", "Sparagus", "Salad", "Carrots", "Tomatoes"}
+list3 = {"WRice", "BRice", "Sweat Potatoes", "Plain potatoes", "Oatmeal", "Quinoa", "Grains", "CousCous"}
+
+print("Comida")
+print(list1[math.random(#list1)])
+print(list2[math.random(#list2)])
+print(list3[math.random(#list3)])
+print("")
+print("Cena")
+print(list1[math.random(#list1)])
+print(list2[math.random(#list2)])
+print(list3[math.random(#list3)])
